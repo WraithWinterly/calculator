9c0339483025143a045d071608541410fa29a0f9
WraithWinterly <37941646+WraithWinterly@users.noreply.github.com>
2022-09-28 23:08:57 -0400

    Angle fixes and UI improvements

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import "./input.css";
 
-import { evaluate } from "mathjs";
+import { create, all } from "mathjs";
+
+import utilsImp from "./utils";
+
+const utils = new utilsImp();
+
+const math = create(all, {});
 
 const buttons = document.querySelectorAll("[data-button]");
 const inputDisplay = document.getElementById("input");
@@ -39,17 +45,14 @@ modeButton.addEventListener("click", () => {
   } else if (mode === "deg") {
     setMode("rad");
   }
-  console.log("update")
+  console.log("update");
   updateCalc();
 });
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    if (getOS() == "iOS" || getOS() == "Android") {
-      inputDisplay.value += e.target.getAttribute("data-button");
-      updateCalc();
-      return;
-    } else {
+    console.log(utils);
+    if (utils.isDesktop()) {
       prevCaretPos = inputDisplay.selectionStart || 0;
       inputDisplay.value = [
         inputDisplay.value.slice(0, prevCaretPos),
@@ -64,8 +67,14 @@ buttons.forEach((button) => {
         prevCaretPos + e.target.getAttribute("data-button").length
       );
       if (button.hasAttribute("data-function")) {
-        // inputDisplay.setSelectionRange(inputDisplay.selectionStart - 1, inputDisplay.selectionStart - 1);
+        console.log("is func");
+        inputDisplay.setSelectionRange(inputDisplay.selectionStart - 1, inputDisplay.selectionStart - 1);
       }
+      console.log("is desk");
+    } else {
+      inputDisplay.value += e.target.getAttribute("data-button");
+      updateCalc();
+      return;
     }
   });
 });
@@ -74,11 +83,11 @@ inputDisplay.addEventListener("keyup", () => {
   updateCalc();
 });
 
-if (!(getOS() == "iOS" || getOS() == "Android")) {
+if (utils.isDesktop()) {
   inputDisplay.focus();
 }
 
-if (!(getOS() == "iOS" || getOS() == "Android")) {
+if (utils.isDesktop()) {
   inputDisplay.addEventListener("blur", (e) => {
     {
       inputDisplay.focus();
@@ -102,7 +111,7 @@ delButton.addEventListener("click", () => {
 
 equals.addEventListener("click", () => {
   if (outputDisplay.innerText === "" && inputDisplay.value !== "") {
-    outputDisplay.innerText = "ERROR";
+    outputDisplay.innerText = "Error";
     return;
   }
   inputDisplay.value = outputDisplay.innerText;
@@ -129,52 +138,91 @@ function updateCalc() {
     valStr = valStr.slice(0, -1);
   }
 
-  
-  if (mode === "deg") {
-    valStr = valStr.replaceAll(/\b(asin|acos|atan)\b\(([^)]+)\)/g, "($1($2) * 180 / pi)");
-    valStr = valStr.replaceAll(/\b(sin|cos|tan)\b(?<!asin|acos|atan)\(([^)]+)\)/g, "$1($2 / 360 * 2 * pi)");
-
-  } else if (mode === "grad") {
-    valStr = valStr.replaceAll(/(asin|acos|atan)\(([^)]+)\)/g, "($1($2) * 200 / pi)");
-    valStr = valStr.replaceAll(/\b(sin|cos|tan)\b(?<!asin|acos|atan)\(([^)]+)\)/g, "$1($2 / 400 * 2 * pi)");
-  }
-
   valStr = valStr.replaceAll(/log10\((\d+)\)/g, "log($1, 10)");
 
-  devInferredInput.innerText = `Input: ${valStr}`;
+  devInferredInput.innerText = valStr;
 
   try {
-    let evaluation = String(evaluate(valStr));
-    devOutput.innerText = `Output: ${evaluation}`;
+    let evaluation = String(math.evaluate(valStr));
+    devOutput.innerText = evaluation;
     if (/^[0-9\.\-\+i\s]+/.test(evaluation) || evaluation.toLowerCase() === "nan" || evaluation === "") {
-      outputDisplay.innerText = evaluate(valStr);
+      outputDisplay.innerText = math.evaluate(valStr);
     } else {
       outputDisplay.innerText = "";
     }
   } catch {
+    devOutput.innerText = `Output: Error`;
     outputDisplay.innerText = "";
   }
 }
 
-function getOS() {
-  var userAgent = window.navigator.userAgent,
-    platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
-    macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"],
-    windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"],
-    iosPlatforms = ["iPhone", "iPad", "iPod"],
-    os = null;
+// config
 
-  if (macosPlatforms.indexOf(platform) !== -1) {
-    os = "Mac OS";
-  } else if (iosPlatforms.indexOf(platform) !== -1) {
-    os = "iOS";
-  } else if (windowsPlatforms.indexOf(platform) !== -1) {
-    os = "Windows";
-  } else if (/Android/.test(userAgent)) {
-    os = "Android";
-  } else if (/Linux/.test(platform)) {
-    os = "Linux";
-  }
+let replacements = {};
 
-  return os;
-}
+// our extended configuration options
+const config = {
+  angles: "deg", // 'rad', 'deg', 'grad'
+};
+
+// create trigonometric functions replacing the input depending on angle config
+const fns1 = ["sin", "cos", "tan", "sec", "cot", "csc"];
+
+fns1.forEach(function (name) {
+  const fn = math[name]; // the original function
+
+  const fnNumber = function (x) {
+    // convert from configured type of angles to radians
+    switch (mode) {
+      case "deg":
+        return fn((x / 360) * 2 * Math.PI);
+      case "grad":
+        return fn((x / 400) * 2 * Math.PI);
+      default:
+        return fn(x);
+    }
+  };
+
+  // create a typed-function which check the input types
+  replacements[name] = math.typed(name, {
+    number: fnNumber,
+    "Array | Matrix": function (x) {
+      return math.map(x, fnNumber);
+    },
+  });
+});
+
+// create trigonometric functions replacing the output depending on angle config
+const fns2 = ["asin", "acos", "atan", "atan2", "acot", "acsc", "asec"];
+fns2.forEach(function (name) {
+  const fn = math[name]; // the original function
+
+  const fnNumber = function (x) {
+    const result = fn(x);
+
+    if (typeof result === "number") {
+      // convert to radians to configured type of angles
+      switch (mode) {
+        case "deg":
+          return (result / 2 / Math.PI) * 360;
+        case "grad":
+          return (result / 2 / Math.PI) * 400;
+        default:
+          return result;
+      }
+    }
+
+    return result;
+  };
+
+  // create a typed-function which check the input types
+  replacements[name] = math.typed(name, {
+    number: fnNumber,
+    "Array | Matrix": function (x) {
+      return math.map(x, fnNumber);
+    },
+  });
+});
+
+// import all replacements into math.js, override existing trigonometric functions
+math.import(replacements, { override: true });
